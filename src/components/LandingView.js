36e7580cded38f09ccaf24767365d34a1794c560
Francisco Benedict <franciscobenedict@gmail.com>
2020-10-22 10:17:49 +0100

    updated copyright of the images used

--- src/components/LandingView.js
+++ src/components/LandingView.js
@@ -13,6 +13,8 @@ const LandingView = () => {
     setPageTitle('Landing page');
   }, [pageTitle]);
 
+  const [toggleTechUsed, setToggleTechUsed] = useState(false);
+
   return (
     <>
       <Navigation />
@@ -20,6 +22,23 @@ const LandingView = () => {
         <h1>{pageTitle}</h1>
         <p>Login with Email or Google.</p>
         <p>{ currentUser ? <>You are logged in</> : <>You are not logged in.</> }</p>
+
+        <div className="profile_buttons_container">
+          <button className="button submit_btn form_button" onClick={() => setToggleTechUsed(!toggleTechUsed)}>Technology used</button>
+        </div>
+
+        {
+          toggleTechUsed &&
+          <ul>
+            <li>react</li>
+            <li>javascript</li>
+            <li>firebase</li>
+            <li>firebase-authentication</li>
+            <li>scss</li>
+            <li>css3</li>
+            <li>html5</li>
+          </ul>
+        }
       </main>
     </>
   )
